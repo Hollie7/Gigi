8cfa2ab2cf6ee75c7073e9484dcc41c73b140dba
iianjy <jiayilian@zju.edu.cn>
2020-07-06 02:42:10 +0800

    charlist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,30 +11,38 @@ class GiGiApp extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+
   render() {
     return (
-      <div>
+      <div className="App">
+      <header className="App-header">
         <h3>GiGi:Hello!</h3>
         <ChatList items={this.state.items} />
         <form onSubmit={this.handleSubmit}>
           {/*<label htmlFor="new-todo">
             What needs to be done?
           </label>*/}
-          <input
+          <textarea class = "inputLine" ref={(input)=>this.inputRef=(input)}
             id="new-todo"
             onChange={this.handleChange}
             value={this.state.text}
           />
-          <button>
+          <button class = "button" ref={(button)=>this.buttonRef=(button)}>
             Send
           </button>
         </form>
+        </header>
       </div>
     );
   }
 
   handleChange(e) {
     this.setState({ text: e.target.value });
+    if (this.inputRef) {
+        console.log('resizing...')
+        this.inputRef.style.height = 'auto';
+        this.inputRef.style.height = this.inputRef.scrollHeight + 'px';
+    }
   }
 
   handleSubmit(e) {
@@ -45,7 +53,7 @@ class GiGiApp extends React.Component {
     const newItem = {
       text: this.state.text,
       id: Date.now(),
-      isGiGi: false
+      isGiGi: Date.now()%2,
     };
     this.setState(state => ({
       items: state.items.concat(newItem),
@@ -55,17 +63,28 @@ class GiGiApp extends React.Component {
 }
 
 class ChatList extends React.Component {
+
+  componentDidMount() {
+    this.contentNode.scrollTop = this.contentNode.scrollHeight;
+  }
+
+  componentDidUpdate() {
+    this.contentNode.scrollTop = this.contentNode.scrollHeight;
+  }
+
   render() {
 
     const chars = this.props.items.map(item => {
-      const desc = item.isGiGi ?'GiGi:' :'Me';      
-      return (
-        <li key={item.id}><p>{desc}: {item.text}</p></li>
-      );
+      // const desc = item.isGiGi ?'GiGi' :'Me';
+      if (item.isGiGi)      
+        return (<div key={item.id} className = "Chat-GiGi" contenteditable> <span > {item.text} </span></div>);
+      else 
+        return (<div key={item.id} className = "Chat-Me" contenteditable> <span > {item.text} </span></div>);
     }); 
+    
 
     return (
-      <ul> {chars} </ul>
+      <div className = "Chat-List" ref={ (node) => this.contentNode = (node) }> {chars} </div>
     );
   }
 }
