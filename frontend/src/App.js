11967c8852748263b1d7d5281a0b7aa75d4d36d5
miaochenlu <3170102668@zju.edu.cn>
2020-07-07 20:38:04 +0800

    login &  register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,92 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
-class GiGiApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], text: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
+import Login from './components/Login'
+import Register from './components/Register'
+import Profile from './components/Profile'
 
-
+class App extends Component {
   render() {
     return (
-      <div className="App">
-      <header className="App-header">
-        <h3>GiGi:Hello!</h3>
-        <ChatList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          {/*<label htmlFor="new-todo">
-            What needs to be done?
-          </label>*/}
-          <textarea class = "inputLine" ref={(input)=>this.inputRef=(input)}
-            id="new-todo"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <button class = "button" ref={(button)=>this.buttonRef=(button)}>
-            Send
-          </button>
-        </form>
-        </header>
-      </div>
-    );
-  }
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing}/>
 
-  handleChange(e) {
-    this.setState({ text: e.target.value });
-    if (this.inputRef) {
-        console.log('resizing...')
-        this.inputRef.style.height = 'auto';
-        this.inputRef.style.height = this.inputRef.scrollHeight + 'px';
-    }
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.text.length === 0) {
-      return;
-    }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now(),
-      isGiGi: Date.now()%2,
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/profile" component={Profile}/>
+          </div>
+        </div>
+      </Router>
+    )
   }
 }
 
-class ChatList extends React.Component {
-
-  componentDidMount() {
-    this.contentNode.scrollTop = this.contentNode.scrollHeight;
-  }
-
-  componentDidUpdate() {
-    this.contentNode.scrollTop = this.contentNode.scrollHeight;
-  }
-
-  render() {
-
-    const chars = this.props.items.map(item => {
-      // const desc = item.isGiGi ?'GiGi' :'Me';
-      if (item.isGiGi)      
-        return (<div key={item.id} className = "Chat-GiGi" contenteditable> <span > {item.text} </span></div>);
-      else 
-        return (<div key={item.id} className = "Chat-Me" contenteditable> <span > {item.text} </span></div>);
-    }); 
-    
-
-    return (
-      <div className = "Chat-List" ref={ (node) => this.contentNode = (node) }> {chars} </div>
-    );
-  }
-}
-
-export default GiGiApp;
+export default App
---
+++ frontend/src/GigiApp.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import '../App.css';
+
+
+class GiGiApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  render() {
+    return (
+      <div className="GigiApp">
+      <header className="GigiApp-header">
+        <h3>GiGi:Hello!</h3>
+        <ChatList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          {/*<label htmlFor="new-todo">
+            What needs to be done?
+          </label>*/}
+          <textarea class = "inputLine" ref={(input)=>this.inputRef=(input)}
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button class = "button" ref={(button)=>this.buttonRef=(button)}>
+            Send
+          </button>
+        </form>
+        </header>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+    if (this.inputRef) {
+        console.log('resizing...')
+        this.inputRef.style.height = 'auto';
+        this.inputRef.style.height = this.inputRef.scrollHeight + 'px';
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now(),
+      isGiGi: Date.now()%2,
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+class ChatList extends React.Component {
+
+  componentDidMount() {
+    this.contentNode.scrollTop = this.contentNode.scrollHeight;
+  }
+
+  componentDidUpdate() {
+    this.contentNode.scrollTop = this.contentNode.scrollHeight;
+  }
+
+  render() {
+
+    const chars = this.props.items.map(item => {
+      // const desc = item.isGiGi ?'GiGi' :'Me';
+      if (item.isGiGi)      
+        return (<div key={item.id} className = "Chat-GiGi" contenteditable> <span > {item.text} </span></div>);
+      else 
+        return (<div key={item.id} className = "Chat-Me" contenteditable> <span > {item.text} </span></div>);
+    }); 
+    
+
+    return (
+      <div className = "Chat-List" ref={ (node) => this.contentNode = (node) }> {chars} </div>
+    );
+  }
+}
+
+export default GiGiApp;
